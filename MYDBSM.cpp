0498cf77b412c7dc0fa57040c04b6c4063c12144
ErFensizec <129927141+ErFensizec@users.noreply.github.com>
2025-01-09 10:05:37 +0800

    Add files via upload
    新增了表格展示数据库格式的相关代码

--- MYDBSM.cpp
+++ MYDBSM.cpp
@@ -1,6 +1,8 @@
 #include "MYDBSM.h"
 #include "ui_MYDBSM.h"
 #include <string>
+#include <algorithm>
+#include <regex>
 #include "reader.h"
 using namespace std;
 
@@ -31,7 +33,51 @@ void MYDBSM::onButtonChoiceClicked()
         ui.label_root->setText(filePath);
         refreshTableFromFile();
     }
-    
+
+
+    ifstream inFile(filePath.toStdString());
+    if (!inFile.is_open()) {
+        QMessageBox::information(this, "Alert", "无法打开数据库文件");
+        return;
+    }
+    try {
+        json dbJson;
+        inFile >> dbJson;
+        inFile.close();
+        int rowNo = 0;
+        ui.table_result->setRowCount(0);
+        ui.table_result->setColumnCount(6);
+        ui.table_result->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch); // 平分
+        ui.table_result->horizontalHeader()->setVisible(true);
+        ui.table_result->setHorizontalHeaderItem(0, new QTableWidgetItem("TableName"));
+        ui.table_result->setHorizontalHeaderItem(1, new QTableWidgetItem("FieldName"));
+        ui.table_result->setHorizontalHeaderItem(2, new QTableWidgetItem("Type"));
+        ui.table_result->setHorizontalHeaderItem(3, new QTableWidgetItem("isKey"));
+        ui.table_result->setHorizontalHeaderItem(4, new QTableWidgetItem("allowNull"));
+        ui.table_result->setHorizontalHeaderItem(5, new QTableWidgetItem("isValid"));
+
+        for (auto& tableJson : dbJson["tables"]) {
+            int columnNo = 0;
+            //ui.table_result->setRowCount(ui.table_result->rowCount() + 1);
+            for (auto& field : tableJson["fields"]) {
+                ui.table_result->setRowCount(ui.table_result->rowCount() + 1);
+                rowNo++;
+                //QMessageBox::information(this, "Help", QString::fromStdString(string(tableJson["table_name"])));
+                ui.table_result->setItem(rowNo - 1, 0, new QTableWidgetItem(QString::fromStdString(tableJson["table_name"])));
+                ui.table_result->setItem(rowNo - 1, 1, new QTableWidgetItem(QString::fromStdString(field["name"])));
+                ui.table_result->setItem(rowNo - 1, 2, new QTableWidgetItem(QString::fromStdString(field["type"])));
+                ui.table_result->setItem(rowNo - 1, 3, new QTableWidgetItem(QString::fromStdString(field["key_flag"])));
+                ui.table_result->setItem(rowNo - 1, 4, new QTableWidgetItem(QString::fromStdString(field["null_flag"])));
+                ui.table_result->setItem(rowNo - 1, 5, new QTableWidgetItem(QString::fromStdString(field["valid_flag"])));
+            }
+        }
+    }
+    catch (...) {
+        QMessageBox::information(this, "Alert", "Invalid Database!");
+        ui.table_result->horizontalHeader()->setVisible(false);
+        ui.table_result->setRowCount(0);
+        ui.table_result->setColumnCount(6);
+    }
 }
 bool endsWith(const std::string& str, const std::string& suffix) {
     if (str.length() >= suffix.length()) {
@@ -70,8 +116,66 @@ void MYDBSM::refreshTableFromFile() {
     
 
 }
+
+// 去除字符串末尾的多余空格
+std::string rtrim(const std::string& str) {
+    size_t end = str.find_last_not_of(" \t\r\n");
+    if (end == std::string::npos)
+        return "";
+    return str.substr(0, end + 1);
+}
+
+// 将字符串中多个空格替换为一个空格
+std::string compressSpaces(const std::string& str) {
+    std::string result;
+    bool in_space = false;
+
+    for (char ch : str) {
+        if (ch == ' ') {
+            if (!in_space) {
+                result += ' ';
+                in_space = true;
+            }
+        }
+        else {
+            result += ch;
+            in_space = false;
+        }
+    }
+    return result;
+}
+
+// 处理字符串，去除每行的多余空格
+std::string processString(const std::string& input) {
+    std::istringstream stream(input);
+    std::string line;
+    std::string result;
+
+    while (std::getline(stream, line)) {
+        // 去除每行末尾的多余空格
+        line = rtrim(line);
+
+        // 压缩中间多个空格为一个空格
+        line = compressSpaces(line);
+
+        // 添加处理后的行到结果中，并添加换行符
+        result += line + "\n";
+    }
+
+    // 删除结果末尾的换行符
+    while (!result.empty() && result[result.size() - 1] == '\n') {
+        result = result.substr(0, result.size() - 1);
+    }
+
+    return result;
+}
+
+
+
+
 void MYDBSM::onCommitCommand() {
-    vector<string> res = streamParse(ui.text_command->document()->toRawText().toStdString());
+    string temp = ui.text_command->document()->toPlainText().toStdString();
+    vector<string> res = streamParse(processString(temp));
     for (int i = 0; i < res.size(); i++) {
         ui.list_info->addItem(QString::fromStdString(res[i]));
         //QMessageBox::information(this, "Help", QString::fromStdString(res[i]));
